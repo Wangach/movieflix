8c5f50ee2666d8cd785c257aa63f53d2d42c2c1e
John Kimani <jwangach@gmail.com>
2024-04-16 23:28:38 +0300

    Bearerremoved

--- src/componentss/MovieList.jsx
+++ src/componentss/MovieList.jsx
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 const MovieList = () => {
     const baseURL = 'https://api.themoviedb.org/3/discover/movie';
-    const API_KEY = 'c362cf6329917fe00c073fab72664e3c';
     const options = {
         headers: {
             accept: 'application/json',
